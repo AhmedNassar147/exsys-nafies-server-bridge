c30ce00e68b0dea6e6a7af2a00eac7934b425522
ahmed.nassar <ahmed.nassar@daybreakhotels.com>
2023-04-30 12:38:01 +0200

    work on rasd-xml

--- packages/bridge/src/helpers/getCurrentDate.mjs
+++ packages/bridge/src/helpers/getCurrentDate.mjs
@@ -3,15 +3,33 @@
  * Helper: `getCurrentDate`.
  *
  */
+
+const formalizeValue = (value) => {
+  const strValue = value.toString();
+  return value < 10 ? `0${strValue}` : strValue;
+};
+
 const getCurrentDate = () => {
-  const date = new Date();
-  const [dateString, time] = date.toLocaleString().split(", ");
-  const [month, day, year] = dateString.split("/");
+  const ts = Date.now();
+
+  const date_ob = new Date(ts);
+  const date = date_ob.getDate();
+  const year = date_ob.getFullYear();
+  const month = formalizeValue(date_ob.getMonth() + 1);
+  const hours = formalizeValue(date_ob.getHours());
+  const minutes = formalizeValue(date_ob.getMinutes());
+  const seconds = formalizeValue(date_ob.getSeconds());
+  const amOrPm = date_ob.getHours() < 12 ? "AM" : "PM";
+
+  const time = `${hours}:${minutes}:${seconds} ${amOrPm}`.toLowerCase();
+  const dateString = `${month}/${date}/${year}`;
 
   return {
-    time: time.toLowerCase(),
-    dateString: `${day}-${month}-${year}`,
+    time,
+    dateString,
   };
 };
+
+getCurrentDate();
 
 export default getCurrentDate;
