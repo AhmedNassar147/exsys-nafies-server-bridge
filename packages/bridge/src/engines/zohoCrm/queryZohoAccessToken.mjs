8f087816bae134a06d8641f5b6d0d90f6eca9e95
ahmed.nassar <ahmed.nassar@daybreakhotels.com>
2023-11-19 12:54:53 +0200

    fix zoho token

--- packages/bridge/src/engines/zohoCrm/queryZohoAccessToken.mjs
+++ packages/bridge/src/engines/zohoCrm/queryZohoAccessToken.mjs
@@ -7,7 +7,7 @@
 import createAxiosPostRequest from "../../helpers/createAxiosPostRequest.mjs";
 
 const apiUrl =
-  "https://accounts.zoho.com/oauth/v2/token?client_id=1000.10SRQLOLF3F0CC84QDW6ZVGF87AKDL&refresh_token=1000.f9fa1e2357ebc11ee0c9922052b8b998.e9e34736e51e9f760987b72575dcfec0&client_secret=428719e9f54c48b801314e30ada236e3b7c4a23a9c&grant_type=refresh_token";
+  "https://accounts.zoho.com/oauth/v2/token?client_id=1000.10SRQLOLF3F0CC84QDW6ZVGF87AKDL&refresh_token=1000.e4c2774934aaffae539f488a00e1d8bf.82d05773477504ce6f5ec745061ce1ce&client_secret=428719e9f54c48b801314e30ada236e3b7c4a23a9c&grant_type=refresh_token";
 
 const queryZohoAccessToken = async () => {
   const { response } = await createAxiosPostRequest({
@@ -26,5 +26,3 @@ const queryZohoAccessToken = async () => {
 };
 
 export default queryZohoAccessToken;
-
-await queryZohoAccessToken();
